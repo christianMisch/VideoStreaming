6a292dd76c9a40ad785c008e8b2edd20c147902d
Christian Mischok <mischok.christian@gmail.com>
2020-06-12 17:15:55 +0200

    changed base image to nodejs image, created server file to serve files from dist folder

---
+++ engine-server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const process = require('process')
+const app = express();
+
+const port = 9000;
+
+app.use('/', express.static(__dirname + '/dist'));
+
+app.listen(port, (_) => {
+    console.log(`Node server is running on port ${port}`);
+});
+
+// docker container can be closed with CTRL + C
+process.on('SIGINT', () => {
+  console.info("Interrupted");
+  process.exit(0);
+});
