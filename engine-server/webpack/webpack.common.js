721fd026eab155e74f5ff45b561a49479cd7b177
Christian Mischok <mischok.christian@gmail.com>
2020-06-12 17:34:08 +0200

    added webpack conf file for production, new webpack folder to distinguish between dev and prod, updated script commands in package.json

--- engine-server/webpack/webpack.common.js
+++ engine-server/webpack/webpack.common.js
@@ -3,15 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true,
-    port: 8888
   },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
