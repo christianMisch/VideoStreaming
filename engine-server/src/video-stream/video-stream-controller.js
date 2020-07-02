d2948e5e30ee6bd369128f75741879b09797c6c7
Christian Mischok <mischok.christian@gmail.com>
2020-07-02 14:50:32 +0200

    refactored video stream controller

--- engine-server/src/video-stream/video-stream-controller.js
+++ engine-server/src/video-stream/video-stream-controller.js
@@ -1,29 +1,8 @@
 export class VideoStreamController {
 
   initVideoStream(scene, engine) {
-    var url = "https://cdn.dashjs.org/latest/dash.all.min.js";
-    var s = document.createElement("script");
-    s.src = url;
-    document.head.appendChild(s);
-
-    // let activeVideoIdx = 1;
-
-    // engine.displayLoadingUI();
-    var stream1 = "http://localhost:8080/playlist.mpd";
-    var stream2 = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
-    // var stream3 = "https://irtdashreference-i.akamaihd.net/dash/live/901161/bfs/manifestARD.mpd";
-    // var stream4 = "https://irtdashreference-i.akamaihd.net/dash/live/901161/bfs/manifestBR.mpd";
-
-    var video1 = $("<video data-dashjs-player autoplay src='" + stream1 + "'></video>");
-    var video2 = $("<video data-dashjs-player src='" + stream2 + "'></video>");
-
-    $("body").append(video1);
-    $("body").append(video2);
-    console.log("Adding HTML video element");
-
-
+    this.appendVideoElements();
     // This is where you create and manipulate meshes
-    console.log('video-controller', scene)
     var myPlane = BABYLON.MeshBuilder.CreatePlane("myPlane", { width: 5.1, height: 3 }, scene);
     myPlane.rotate(BABYLON.Axis.X, Math.PI, BABYLON.Space.WORLD);
 
@@ -88,6 +67,27 @@ export class VideoStreamController {
     //   mat.emissiveTexture = videoTexture;
     //   plane.material = mat;
     // }, { maxWidth: 256, maxHeight: 256 });
+  }
+
+  appendVideoElements() {
+    const url = "https://cdn.dashjs.org/latest/dash.all.min.js";
+    const script = $('<script>').attr('src', url);
+    $('head').append(script);
+
+    // let activeVideoIdx = 1;
+
+    // engine.displayLoadingUI();
+    const stream1 = 'http://localhost:8080/playlist.mpd';
+    const stream2 = 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd';
+    // const stream3 = 'https://irtdashreference-i.akamaihd.net/dash/live/901161/bfs/manifestARD.mpd';
+    // const stream4 = 'https://irtdashreference-i.akamaihd.net/dash/live/901161/bfs/manifestBR.mpd';
+
+    const video1 = $('<video data-dashjs-player autoplay src="' + stream1 + '"></video>');
+    const video2 = $('<video data-dashjs-player src="' + stream2 + '"></video>');
+
+    $('body').append(video1);
+    $('body').append(video2);
+    console.log('Adding HTML video element');
   }
 
 }
